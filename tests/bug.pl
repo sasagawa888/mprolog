a655089bc2620979c297ee35f9addc0d37fe55fe
sasagawa888 <ken-s@tkcnf.or.jp>
2026-05-31 16:48:16 +0900

    tesing new compiler

--- tests/bug.pl
+++ tests/bug.pl
@@ -7,3 +7,18 @@ p(2).
 foo :- p(X),write(X),fail.
 foo.
 
+n(1). n(2). n(3). n(4). n(5).
+n(6). n(7). n(8). n(9). n(10).
+
+bench :-
+    n(X),
+    fail.
+bench.
+
+bench1 :-
+    n(X),
+    n(Y),
+    n(Z),
+    write([X,Y,Z]),nl,
+    fail.
+bench1.
