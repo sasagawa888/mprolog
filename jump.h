8085544bd8c8f02b0c84d93e51df17aea295bdb9
sasagawa888 <ken-s@tkcnf.or.jp>
2026-06-07 13:25:18 +0900

    add Jrespond

--- jump.h
+++ jump.h
@@ -394,6 +394,12 @@ static inline int Jcopy_work(int x, int th) {
     return f2[COPY_WORK_IDX](x, th);
 }
 
+static inline int Jrespond(int x, int th) {
+    if(x == NIL)
+        return(YES);
+    else 
+        return f2[CRESPOND_IDX](x, th);
+}
 
 
 static inline int Jlist3(int x, int y, int z) {
