5839850e0272980c59e50b3210f16759e2873f17
sasagawa888 <ken-s@tkcnf.or.jp>
2026-06-16 14:17:25 +0900

    fix compiler for fact/2  OK!

--- library/compiler.pl
+++ library/compiler.pl
@@ -492,6 +492,7 @@ gen_nondet_body1((X,Y),A,M,N,B,O,L,H) :-
     gen_nondet_body1(Y,A,M,N1,B,O,L,H),
     write('}'),
     gen_nondet_body_retry(B),nl.
+
 gen_nondet_body1((X,Y),A,M,N,B,O,L,H) :-
     n_property(X,predicate),
     recur_body(X,H),
@@ -501,7 +502,7 @@ gen_nondet_body1((X,Y),A,M,N,B,O,L,H) :-
     write('Jpush_back(th);'),nl,
     gen_nondet_body_label([A,M,N]),
     write('if (c_'),write(P),write('(arg_'),write(A),write('_'),write(M),write('_'),write(N),
-    write(',rest,th) == YES){'),nl,
+    ifthenelse(Y=end_of_body,write(',rest,th) == YES){'),write(',NIL,th) == YES){')),nl,
     N1 is N+1,
     gen_nondet_body1(Y,A,M,N1,[A,M,N],rec,L,H),
     write('}'),
@@ -547,7 +548,8 @@ gen_nondet_body1(end_of_body,A,M,N,B,res,L,H) :-
 gen_nondet_body1(end_of_body,A,M,N,B,rec,L,H) :-
     write('Jcopy_back(th);'),nl,
     write('Jpop_back(th);'),nl,
-    write('if(rest==NIL) return(YES);').
+    write('if(rest==NIL) return(YES);'),
+    write('else if(Jrespond(rest,th)==YES) return(YES);'),nl.
 gen_nondet_body1(X,A,M,N,B,O,L,H) :-
     gen_nondet_body1((X,end_of_body),A,M,N,B,O,L,H).
 
