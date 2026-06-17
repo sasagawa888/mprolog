94b4494b1af869cd8de98fccb24bc234227a47ea
sasagawa888 <ken-s@tkcnf.or.jp>
2026-06-18 06:00:54 +0900

    improve n_property/2

--- extension.c
+++ extension.c
@@ -741,8 +741,11 @@ int b_n_property(int arglist, int rest, int th)
 	    else if (get_arity(arg1) == 3
 		     && unify(arg2, makeconst("compiled_tail"), th) == YES)
 		return (prove_all(rest, sp[th], th));
-	    else if (get_arity(arg1) == 3
-		     && unify(arg2, makeconst("compiled_halt"), th) == YES)
+	    else if (get_arity(arg1) == 4
+		     && unify(arg2, makeconst("compiled_dyn"), th) == YES)
+		return (prove_all(rest, sp[th], th));
+		else if (get_arity(arg1) == 5
+		     && unify(arg2, makeconst("compiled_mut"), th) == YES)
 		return (prove_all(rest, sp[th], th));
 	    else
 		return (NO);
