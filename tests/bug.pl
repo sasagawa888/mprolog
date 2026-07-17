8480a3b556127460f3a57de8c51e83ba66596c54
sasagawa888 <ken-s@tkcnf.or.jp>
2026-07-17 16:19:47 +0900

    update bug.pl

--- tests/bug.pl
+++ tests/bug.pl
@@ -1,3 +1,14 @@
+n(1). n(2). n(3). n(4). n(5).
+n(6). n(7). n(8). n(9). n(10).
+bench :-
+    n(X),
+    n(Y),
+    n(Z),
+    n(A),
+    n(B),
+    fail.
+bench.
+
 
 mappend([],X,X).
 mappend([A|X],Y,[A|Z]) :-
