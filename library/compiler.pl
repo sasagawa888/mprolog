e37ea1131c1fb81ed4b8d0c6080611f55f762869
sasagawa888 <ken-s@tkcnf.or.jp>
2026-07-20 15:32:43 +0900

    fix compiler allfail:  recur==0 return NO

--- library/compiler.pl
+++ library/compiler.pl
@@ -854,6 +854,7 @@ gen_recursion4 :-
 
 gen_recursion5 :-
     write('allfail:'),nl,
+    write('if(Jget_scp(RECUR,th)==0) return(NO);'),nl,
     write('Jpop_recur(th);'),nl,
     write('Jpop_next(th);'),nl,
     write('index = Jget_cont(th);'),nl,
