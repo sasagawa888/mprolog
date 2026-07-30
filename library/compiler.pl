ee6cc0d6df6754fa08cc4d4e77bd552912a1901d
sasagawa888 <ken-s@tkcnf.or.jp>
2026-07-30 16:59:00 +0900

    restore Jinc_choice

--- library/compiler.pl
+++ library/compiler.pl
@@ -892,6 +892,7 @@ gen_recur_pred(P) :-
 % N is arity , M is Mth clause from 0.
 % clause
 gen_a_recur_clause((Head :- Body),A,M,P,V) :-
+    write('Jinc_choice(th);'),nl,
     P =.. [P1|_],
     M1 is M+1,
     write('Jset_back(&&'),gen_recur_clause_label([P1,A,M1]),write(',th);'),nl,
@@ -903,11 +904,13 @@ gen_a_recur_clause((Head :- Body),A,M,P,V) :-
 gen_a_recur_clause(P,A,M,_,_) :-
 	n_property(P,predicate),
     functor(P,_,0),
+    write('Jinc_choice(th);'),nl,
     write('return(YES);}'),nl,!.
 
 % nondet predicate
 gen_a_recur_clause(P,A,M,_,_) :-
 	n_property(P,predicate),
+    write('Jinc_choice(th);'),nl,
     P =.. [P1|_],
     M1 is M+1,
     write('Jset_back(&&'),gen_recur_clause_label([P1,A,M1]),write(',th);'),nl,
