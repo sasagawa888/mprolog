f354451860cc15276ddfd659833778d84f53e495
sasagawa888 <ken-s@tkcnf.or.jp>
2026-08-02 10:15:14 +0900

    update bug.pl

--- tests/bug.pl
+++ tests/bug.pl
@@ -60,3 +60,12 @@ apptest1 :-
     fail.
 
 */
+
+
+likes(kim,robin).
+likes(sandy,lee).
+likes(sandy,kim).
+likes(robin,cats).
+likes(sandy,X) :- likes(X,cats).
+likes(kim,X) :- likes(X,lee),likes(X,kim).
+likes(X,X).
