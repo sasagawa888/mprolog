0d150492118d41f2bda4ee1cc55807f7dda4a531
sasagawa888 <ken-s@tkcnf.or.jp>
2026-08-18 12:12:34 +0900

    debugging T argument

--- library/compiler.pl
+++ library/compiler.pl
@@ -819,7 +819,7 @@ gen_nondet_body1((X,end_of_body),A,M,N,B,H,P,V,T) :-
     M1 is M+1,
     case([B==[] -> true,
           B==cut -> (write('Spush_back(&&allfail,arglist,th);'),nl),
-          B==nil -> true
+          T==det -> true
           |(write('Spush_back(&&'),gen_nondet_body_label([P|B]),write('back,arglist,th);'),nl)]),
     N1 is N+1,
     gen_pack_pointer(V,1),
@@ -840,7 +840,7 @@ gen_nondet_body1((X,Y),A,M,N,B,H,P,V,T) :-
     M1 is M+1,
     case([B==[] -> (write('Spush_back(&&'),gen_nondet_clause_label([P,A,M1]),write(',arglist,th);'),nl),
           B==cut -> (write('Spush_back(&&allfail,arglist,th);'),nl),
-          B==nil -> true
+          T==det -> true
           |(write('Spush_back(&&'),gen_nondet_body_label([P|B]),write('back,arglist,th);'),nl)]),
     write('goto '),gen_nondet_body_label([P,A,M,N]),write('join;'),nl,
     gen_nondet_body_label([P,A,M,N]),write('back:'),nl,
@@ -866,7 +866,7 @@ gen_nondet_body1((X,Y),A,M,N,B,H,P,V,T) :-
     M1 is M+1,
     case([B==[] -> (write('Spush_back(&&'),gen_nondet_clause_label([P,A,M1]),write(',arglist,th);'),nl),
           B==cut -> (write('Spush_back(&&allfail,arglist,th);'),nl),
-          B==nil -> true
+          T==det -> true
           |(write('Spush_back(&&'),gen_nondet_body_label([P|B]),write('back,arglist,th);'),nl)]),
     write('goto '),gen_nondet_body_label([P,A,M,N]),write('join;'),nl,
     gen_nondet_body_label([P,A,M,N]),write('back:'),nl,
@@ -901,7 +901,7 @@ gen_nondet_body1((X,Y),A,M,N,B,H,P,V,T) :-
     n_findatom(Pred,builtin,Num),
     write('subr_number = '),write(Num),write(';'),nl,
     write('goto builtin_call;'),nl,
-    gen_nondet_body1(Y,A,M,N1,nil,H,P,V,nil).
+    gen_nondet_body1(Y,A,M,N1,nil,H,P,V,det).
 
 % det tail
 gen_nondet_body1((X,Y),A,M,N,B,H,P,V,T) :-
