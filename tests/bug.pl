2f948d92e49054c83643ac1d368328e9afffa248
sasagawa888 <ken-s@tkcnf.or.jp>
2026-08-21 06:32:22 +0900

    update bug.pl

--- tests/bug.pl
+++ tests/bug.pl
@@ -1,11 +1,11 @@
 
-between(Low, High, Low) :-
+mbetween(Low, High, Low) :-
     Low =< High.
 
-between(Low, High, X) :-
+mbetween(Low, High, X) :-
     Low < High,
     Low1 is Low + 1,
-    between(Low1, High, X).
+    mbetween(Low1, High, X).
 
 select1(X,[X|Xs],Xs).
 select1(X,[Y|Ys],[Y|Zs]) :-
