2a899301a3c159702f513bd112a5ea0fcf4a7b47
sasagawa888 <ken-s@tkcnf.or.jp>
2026-08-23 06:14:43 +0900

    fix success:  add restore np  between is OK

--- library/compiler.pl
+++ library/compiler.pl
@@ -611,6 +611,7 @@ gen_SCBM_function4 :-
     write('next = back_goto[rp[th]][th];'),nl,
     write('clause = Sget_choice(th);'),nl,
     write('arglist = Sget_arg(th);'),nl,
+    write('np[th] = Sget_np(th);'),nl,
     write('Spush_next(&&success,th);'),nl,
     write('goto *next;'),nl,
     write('}else{'),nl,
